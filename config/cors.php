579c38f86ea277d34e3f0bea225ce7357831f7f5
Febryan Riyadi <febryanriyadi03@gmail.com>
2025-10-30 15:15:18 +0700

    Update

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,9 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:3000'],
+    'allowed_origins' => [
+        env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173'),
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
